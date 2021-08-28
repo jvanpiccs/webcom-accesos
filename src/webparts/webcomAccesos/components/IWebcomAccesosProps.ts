53ade47ec8073bc3405d3e554363588a3c80aa3c
jvanpiccs <9990398+jvanpiccs@users.noreply.github.com>
2021-08-28 00:31:00 -0300

    first commit

---
+++ src/webparts/webcomAccesos/components/IWebcomAccesosProps.ts
@@ -0,0 +1,3 @@
+export interface IWebcomAccesosProps {
+  description: string;
+}
---
+++ src/webparts/webcomAccesos/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWebcomAccesosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WebcomAccesosWebPartStrings' {
+  const strings: IWebcomAccesosWebPartStrings;
+  export = strings;
+}
